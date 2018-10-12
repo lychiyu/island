56e1c41a4ec212381efa5f8d0334725a12e5ec18
liuying <liuyingover@163.com>
2018-10-12 22:06:54 +0800

    完成music组件样式编写

---
+++ components/classic/music/index.js
@@ -0,0 +1,28 @@
+import {classicBeh} from '../classic-beh.js'
+
+Component({
+
+  behaviors: [classicBeh],
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    src: String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    pauseSrc: 'images/player@pause.png',
+    playSrc: 'images/player@play.png',
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
