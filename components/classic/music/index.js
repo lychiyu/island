593e897175e7973c5a6b23bbfb65a8a2d8db0c8a
liuying <liuyingover@163.com>
2018-10-12 23:15:02 +0800

    完成音乐播放

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,4 +1,9 @@
-import {classicBeh} from '../classic-beh.js'
+import {
+  classicBeh
+} from '../classic-beh.js'
+
+// 获取背景音乐管理对象
+const mMgr = wx.getBackgroundAudioManager()
 
 Component({
 
@@ -15,14 +20,60 @@ Component({
    * 组件的初始数据
    */
   data: {
+    playing: false,
     pauseSrc: 'images/player@pause.png',
     playSrc: 'images/player@play.png',
   },
 
+  attached: function () {
+    console.log(1)
+    this._recoverPlayerStatus()
+    this._monitorPlayerSwitch()
+  },
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onPlay: function (event) {
+      if (!this.data.playing) {
+        this.setData({
+          playing: true
+        })
+        mMgr.src = this.properties.src
+      } else {
+        this.setData({
+          playing: false
+        })
+        mMgr.pause()
+      }
+    },
+    _recoverPlayerStatus() {
+      // 恢复播放器状态
+      if (mMgr.paused) {
+        this.setData({
+          playing: false
+        })
+        return
+      }
+      if (mMgr.src == this.properties.src) {
+        this.setData({
+          playing: true
+        })
+      }
+    },
+    _monitorPlayerSwitch: function () {
+      mMgr.onPlay(() => {
+        this._recoverPlayerStatus()
+      })
+      mMgr.onPause(() => {
+        this._recoverPlayerStatus()
+      })
+      mMgr.onEnded(() => {
+        this._recoverPlayerStatus()
+      })
+      mMgr.onStop(() => {
+        this._recoverPlayerStatus()
+      })
+    }
   }
 })
