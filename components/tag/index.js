d0812c724ac304885d18c48736f9b7f7544b15b7
liuying <liuyingover@163.com>
2018-10-15 12:55:57 +0800

    tag组件

---
+++ components/tag/index.js
@@ -0,0 +1,32 @@
+// components/tag/index.js
+Component({
+
+  options: {
+    // 设置组件启用插槽slot
+    multipleSlots: true
+  },
+
+  // 设置外部样式组
+  externalClasses: ['tag-class'],
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    text: String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
